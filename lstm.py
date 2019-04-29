29f0b2e1c97cc7d74cf4c9164c83d142726525e8
scheng123 <simoncheng559@gmail.com>
2019-04-29 19:23:19 -0400

    Enable feature of loading weights

--- lstm.py
+++ lstm.py
@@ -11,6 +11,7 @@ from keras.layers import LSTM
 from keras.layers import Activation
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
 def train_network():
     """ Train a Neural Network to generate music """
@@ -85,22 +86,25 @@ def prepare_sequences(notes, n_vocab):
     return (network_input, network_output)
 
 def create_network(network_input, n_vocab):
-    """ create the structure of the neural network """
-    model = Sequential()
-    model.add(LSTM(
-        512,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
-        return_sequences=True
-    ))
-    model.add(Dropout(0.3))
-    model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(512))
-    model.add(Dense(256))
-    model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    try:
+        model = load_model(sys.argv[2])
+    except NameError:
+        """ create the structure of the neural network """
+        model = Sequential()
+        model.add(LSTM(
+            512,
+            input_shape=(network_input.shape[1], network_input.shape[2]),
+            return_sequences=True
+        ))
+        model.add(Dropout(0.3))
+        model.add(LSTM(512, return_sequences=True))
+        model.add(Dropout(0.3))
+        model.add(LSTM(512))
+        model.add(Dense(256))
+        model.add(Dropout(0.3))
+        model.add(Dense(n_vocab))
+        model.add(Activation('softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     return model
 
