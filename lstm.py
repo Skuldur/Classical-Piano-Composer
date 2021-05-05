60ddadcd7dee30dd03ae6029be85ee13f607bd8f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-05 21:19:41 +0200

    Initial commit

--- lstm.py
+++ lstm.py
@@ -3,8 +3,9 @@
 import glob
 import pickle
 import numpy
+import os
 from music21 import converter, instrument, note, chord
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -13,8 +14,9 @@ from keras.layers import BatchNormalization as BatchNorm
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 
+
 def train_network():
-    """ Train a Neural Network to generate music """
+    """Train a Neural Network to generate music"""
     notes = get_notes()
 
     # get amount of pitch names
@@ -22,12 +24,19 @@ def train_network():
 
     network_input, network_output = prepare_sequences(notes, n_vocab)
 
-    model = create_network(network_input, n_vocab)
+    checkpoints = ["checkpoints/" + name for name in os.listdir("checkpoints/")]
+    if checkpoints:
+        latest_checkpoint = max(checkpoints, key=os.path.getctime)
+        print(f"Restoring from the lastest checkpoint: {latest_checkpoint}")
+        model = load_model(latest_checkpoint)
+    else:
+        model = create_network(network_input, n_vocab)
 
     train(model, network_input, network_output)
 
+
 def get_notes():
-    """ Get all the notes and chords from the midi files in the ./midi_songs directory """
+    """Get all the notes and chords from the midi files in the ./midi_songs directory"""
     notes = []
 
     for file in glob.glob("midi_songs/*.mid"):
@@ -37,31 +46,32 @@ def get_notes():
 
         notes_to_parse = None
 
-        try: # file has instrument parts
+        try:  # file has instrument parts
             s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse() 
-        except: # file has notes in a flat structure
+            notes_to_parse = s2.parts[0].recurse()
+        except:  # file has notes in a flat structure
             notes_to_parse = midi.flat.notes
 
         for element in notes_to_parse:
             if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):
-                notes.append('.'.join(str(n) for n in element.normalOrder))
+                notes.append(".".join(str(n) for n in element.normalOrder))
 
-    with open('data/notes', 'wb') as filepath:
+    with open("data/notes", "wb") as filepath:
         pickle.dump(notes, filepath)
 
     return notes
 
+
 def prepare_sequences(notes, n_vocab):
-    """ Prepare the sequences used by the Neural Network """
+    """Prepare the sequences used by the Neural Network"""
     sequence_length = 100
 
     # get all pitch names
     pitchnames = sorted(set(item for item in notes))
 
-     # create a dictionary to map pitches to integers
+    # create a dictionary to map pitches to integers
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
     network_input = []
@@ -69,7 +79,7 @@ def prepare_sequences(notes, n_vocab):
 
     # create input sequences and the corresponding outputs
     for i in range(0, len(notes) - sequence_length, 1):
-        sequence_in = notes[i:i + sequence_length]
+        sequence_in = notes[i : i + sequence_length]
         sequence_out = notes[i + sequence_length]
         network_input.append([note_to_int[char] for char in sequence_in])
         network_output.append(note_to_int[sequence_out])
@@ -85,42 +95,55 @@ def prepare_sequences(notes, n_vocab):
 
     return (network_input, network_output)
 
+
 def create_network(network_input, n_vocab):
-    """ create the structure of the neural network """
+    """create the structure of the neural network"""
     model = Sequential()
-    model.add(LSTM(
-        512,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
-        recurrent_dropout=0.3,
-        return_sequences=True
-    ))
-    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+    model.add(
+        LSTM(
+            512,
+            input_shape=(network_input.shape[1], network_input.shape[2]),
+            recurrent_dropout=0.3,
+            return_sequences=True,
+        )
+    )
+    model.add(
+        LSTM(
+            512,
+            return_sequences=True,
+            recurrent_dropout=0.3,
+        )
+    )
     model.add(LSTM(512))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
     model.add(Dense(256))
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.add(Activation("softmax"))
+    model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 
     return model
 
+
 def train(model, network_input, network_output):
-    """ train the neural network """
-    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    """train the neural network"""
+    filepath = "checkpoints/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
     checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
+        filepath, monitor="loss", verbose=0, save_best_only=True, mode="min"
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
+    model.fit(
+        network_input,
+        network_output,
+        epochs=200,
+        batch_size=128,
+        callbacks=callbacks_list,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     train_network()
